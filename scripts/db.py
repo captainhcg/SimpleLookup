029d6a485643f366efee1bad061f377204dd3141
Chongguang He <captainhcg@gmail.com>
2013-09-01 17:11:04 -0700

    python source code parser

---
+++ scripts/db.py
@@ -0,0 +1,115 @@
+import sys
+sys.path.append('../')
+import settings
+import sqlite3
+
+DB_NAME = settings.PROJECTS[0]["DB_NAME"]
+def reset(project_id=0):
+    global DB_NAME
+    DB_NAME = settings.PROJECTS[project_id]["DB_NAME"]
+    with sqlite3.connect('../db/%s.db'%DB_NAME) as conn:
+        # Create table
+        c = conn.cursor()
+        c.execute("""
+            DROP TABLE IF EXISTS module;
+            """)
+        c.execute("""
+            CREATE TABLE module(
+                id INTEGER PRIMARY KEY      NOT NULL,
+                name           CHAR(64)     NOT NULL,
+                path           TEXT         NOT NULL
+            )
+            """)
+        c.execute("""
+            DROP TABLE IF EXISTS class;
+            """)
+        c.execute("""
+            CREATE TABLE class(
+                id INTEGER PRIMARY KEY      NOT NULL,
+                name           CHAR(64)     NOT NULL,
+                code           TEXT         NOT NULL,
+                module_id      INTEGER,
+                class_id       INTEGER,
+                FOREIGN KEY(module_id) REFERENCES module(id)
+                FOREIGN KEY(class_id) REFERENCES class(id)
+            )
+            """)
+        c.execute("""
+            DROP TABLE IF EXISTS function;
+            """)
+        c.execute("""
+            CREATE TABLE function(
+                id INTEGER PRIMARY KEY      NOT NULL,
+                name           CHAR(64)     NOT NULL,
+                code           TEXT         NOT NULL,
+                module_id      INTEGER,
+                class_id       INTEGER,
+                function_id    INTEGER,
+                FOREIGN KEY(module_id) REFERENCES module(id)
+                FOREIGN KEY(class_id) REFERENCES class(id)
+                FOREIGN KEY(function_id) REFERENCES function(id)
+            )
+            """)
+        c.execute("""
+            DROP TABLE IF EXISTS attribute;
+            """)
+        c.execute("""
+            CREATE TABLE attribute(
+                id INTEGER PRIMARY KEY      NOT NULL,
+                name           CHAR(64)     NOT NULL,
+                code           TEXT         NOT NULL,
+                module_id      INTEGER,
+                class_id       INTEGER,
+                FOREIGN KEY(module_id) REFERENCES module(id)
+                FOREIGN KEY(class_id) REFERENCES class(id)
+            )
+            """)
+        conn.commit()
+
+def addModule(name, path):
+    last_id = 0
+    with sqlite3.connect('../db/%s.db'%DB_NAME) as conn:
+        c = conn.cursor()
+        c.execute(u"INSERT INTO module (name, path) VALUES ('%s','%s')"%(name, path))
+        last_id = c.lastrowid
+        conn.commit()
+    return last_id
+
+def setClassCode(class_id, code=""):
+    with sqlite3.connect('../db/%s.db'%DB_NAME) as conn:
+        c = conn.cursor()
+        c.execute(u"UPDATE class SET code = ? WHERE id = ?", (code, class_id))
+        conn.commit()
+
+def setFunctionCode(function_id, code=""):
+    with sqlite3.connect('../db/%s.db'%DB_NAME) as conn:
+        c = conn.cursor()
+        c.execute(u"UPDATE function SET code = ? WHERE id = ?", (code, function_id))
+        conn.commit()   
+
+def addAttribute(name, module_id, class_id, code=""):
+    last_id = 0
+    with sqlite3.connect('../db/%s.db'%DB_NAME) as conn:
+        c = conn.cursor()
+        c.execute(u"INSERT INTO attribute (name, module_id, class_id, code) VALUES (?, ?, ?, ?)", (name, module_id or "NULL", class_id or "NULL", code))
+        last_id = c.lastrowid
+        conn.commit()
+    return last_id
+
+def addFunction(name, module_id, class_id, function_id, code=""):
+    last_id = 0
+    with sqlite3.connect('../db/%s.db'%DB_NAME) as conn:
+        c = conn.cursor()
+        c.execute(u"INSERT INTO function (name, module_id, class_id, function_id, code) VALUES (?, ?, ?, ?, ?)", (name, module_id or "NULL", class_id or "NULL", function_id or "NULL", code))
+        last_id = c.lastrowid
+        conn.commit()
+    return last_id
+
+def addClass(name, module_id, class_id, code=""):
+    last_id = 0
+    with sqlite3.connect('../db/%s.db'%DB_NAME) as conn:
+        c = conn.cursor()
+        c.execute(u"INSERT INTO class (name, module_id, class_id, code) VALUES (?, ?, ?, ?)", (name, module_id or "NULL", class_id or "NULL", code))
+        last_id = c.lastrowid
+        conn.commit()
+    return last_id
--- scripts/parser.py
+++ scripts/parser.py
@@ -6,81 +6,98 @@ import traceback
 import inspect
 import ast
 import os, os.path
-
-print settings.PROJECT_PATH
+import db
 
 verbose = False
-sys.path.append(settings.PROJECT_PATH)
 source_code = []
 
-def parseOther(item):
+def parseNode(item, module_id=0, class_id=0, function_id=0):
+    if isinstance(item, ast.FunctionDef):
+        return parseFunction(item, module_id, class_id, function_id)
+    elif isinstance(item, ast.ClassDef):
+        return parseClass(item, module_id, class_id, function_id)
+    else:
+        return parseOther(item, module_id, class_id, function_id)
+
+def parseOther(item, module_id=0, class_id=0, function_id=0):
     lastLine = item.lineno
     if hasattr(item, "body"):
         if not isinstance(item.body, list):
             item.body = [item.body]
         for node in item.body:
-            lastLine = parseNode(node)
+            lastLine = parseNode(node, module_id, class_id, function_id)
     return lastLine
 
-def parseNode(item):
-    if isinstance(item, ast.FunctionDef):
-        return parseFunction(item)
-    elif isinstance(item, ast.ClassDef):
-        return parseClass(item)
-    else:
-        return parseOther(item)
-
-def parseFunction(item):
+def parseFunction(item, module_id=0, class_id=0, function_id=0):
+    global source_code, verbose
+    function_id = db.addFunction(item.name, module_id, class_id, function_id)
     start_line = item.lineno
     end_line = item.lineno
     for node in item.body:
-        end_line = parseNode(node)
+        end_line = parseNode(node, module_id, class_id, function_id)
 
     if verbose:
         print "function: %s"%item.name
         print start_line, end_line
-    code = "".join(source_code[start_line: end_line+1])
+    code = u"".join(source_code[start_line: end_line+1])
+    db.setFunctionCode(function_id, code)
     return end_line
 
-def parseClass(item):
+def parseClass(item, module_id=0, class_id=0, function_id=0):
+    global source_code, verbose
+    class_id = db.addClass(item.name, module_id, class_id)
     start_line = item.lineno
     end_line = item.lineno
     for node in item.body:
-        end_line = parseNode(node)
-
+        end_line = parseNode(node, module_id, class_id, function_id)
+        if isinstance(node, ast.Assign):
+            db.addAttribute(item.name, module_id, class_id, source_code[node.lineno])
     if verbose:
         print "class: %s"%item.name
         print start_line, end_line
     code = "".join(source_code[start_line: end_line+1])
-    print "st:%d"%start_line, code, end_line
+    db.setClassCode(class_id, code)
     return end_line
 
-def parseModule(source):
+def parseModule(source, module_id=0):
     tree = ast.parse(source)
     if not hasattr(tree, "body"):
         return
     for item in tree.body:
         if isinstance(item, ast.FunctionDef):
-            parseFunction(item)
+            parseFunction(item, module_id)
         elif isinstance(item, ast.ClassDef):  
-            parseClass(item)
+            parseClass(item, module_id)
 
-for root, dirs, files in os.walk(settings.PROJECT_PATH):
-    for f in files:
-        if f.endswith(settings.FILE_EXTENSION):
-            fullpath = os.path.join(root, f)
-            source_code = []
-            print fullpath
-            try:
-                # (name, suffix, mode, mtype) = inspect.getmoduleinfo(fullpath)
-                # print (name, suffix, mode, mtype)
-                with open(fullpath, "rb") as f:
-                    source_code.append("")
-                    for line in f:
-                       source_code.append(line) 
-                with open(fullpath, "rb") as f:
-                    parseModule(f.read())
-            except Exception as e:
-                traceback.print_exc()
-                exit()
-            # print fullpath
+def main(argv=sys.argv):
+    global source_code
+    if len(argv)<2:
+        project_id = 0
+    else:
+        try:
+            project_id = int(argv[1])
+        except:
+            project_id = 0
+    PROJECT_PATH = settings.PROJECTS[project_id]["PROJECT_PATH"]
+    db.reset(project_id=project_id)
+    for root, dirs, files in os.walk(PROJECT_PATH):
+        name_offset = len(settings.FILE_EXTENSION)
+        path_offset = len(PROJECT_PATH)
+        for f in files:
+            if f.endswith(settings.FILE_EXTENSION):
+                fullpath = os.path.join(root, f)
+                source_code = []
+                module_id = db.addModule(f[:-name_offset], root[path_offset:])
+                try:
+                    with open(fullpath, "rb") as f:
+                        source_code.append("")
+                        for line in f:
+                           source_code.append(line.decode('utf-8')) 
+                    with open(fullpath, "rb") as f:
+                        parseModule(f.read(), module_id=module_id)
+                except Exception as e:
+                    traceback.print_exc()
+                    exit()
+
+if __name__ == "__main__":
+    main()
