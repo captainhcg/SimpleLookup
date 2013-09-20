194f115a2897397cc89952320262e2b5695625bf
captainhcg <captainhcg@gmail.com>
2013-09-20 16:44:18 -0700

    bugfix of coffee script

--- search_app/views.py
+++ search_app/views.py
@@ -24,7 +24,7 @@ common_js = Bundle(
 assets.register('common_js', common_js)
 common_css = Bundle('css/style.css', filters='cssmin', output='common_css.css')
 assets.register('common_css', common_css)
-assets.config["auto_build"] = True
+assets.config["auto_build"] = False
 
 def init_global(function):
     """alway pass project_id to view"""
