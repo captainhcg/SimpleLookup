ffb47ac7bc0863d2ad8c62a831cdbb710e0f66fd
Chongguang He <captainhcg@gmail.com>
2013-09-22 03:38:57 -0700

    ...

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
-from search_app import app
+from flask import Flask
+from werkzeug.wsgi import DispatcherMiddleware
+from search_app import app as entrance
+from track_app import app as track
 import settings
 
 if __name__ == '__main__':
+    app = Flask( __name__ )
+    app.wsgi_app = DispatcherMiddleware(entrance.wsgi_app, {
+        '/track': track.wsgi_app
+    })
+
     app.run(
         host=settings.APP_HOST,
         port=settings.APP_PORT,
